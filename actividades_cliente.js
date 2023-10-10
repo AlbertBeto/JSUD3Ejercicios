7faa21ea67186d970b3844dab66a87d7fb4fe2ab
Albert <albertweb08@gmail.com>
2023-10-10 20:02:52 +0200

    Acabo el ejercicio 3.1

--- actividades_cliente.js
+++ actividades_cliente.js
@@ -1,3 +1,4 @@
 'use strict'
-
+// 3.1 Uso la función document.write directamente en el JS y simplemente llamo el script en el html
+//ya que al momento del ejercicio solo hay este codigo. 
 document.write("<p>Tempestes venen del <strong>sud</strong>.</p>");
