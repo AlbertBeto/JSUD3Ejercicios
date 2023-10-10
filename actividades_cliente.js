1de5c0c3893a10edf5e0a92fb43e81bc71594c49
Albert <albertweb08@gmail.com>
2023-10-10 20:16:24 +0200

    incorporo imagenes.

--- actividades_cliente.js
+++ actividades_cliente.js
@@ -1,4 +1,10 @@
 'use strict'
 // 3.1 Uso la función document.write directamente en el JS y simplemente llamo el script en el html
 //ya que al momento del ejercicio solo hay este codigo. 
-document.write("<p>Tempestes venen del <strong>sud</strong>.</p>");
+
+//document.write("<p>Tempestes venen del <strong>sud</strong>.</p>");
+
+
+
+// 3.2 Usando prompts y document.write() pide datos e imprime en pantalla 
+// 3.2.1 Prgunta si quiere ver perro o gato y muestra en pantalla la imagen de perro o gato. 
